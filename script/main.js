334a33fc1626204c79ab9de9e8e40ce336db2d13
Syllac21 <syllac.online@gmail.com>
2024-10-22 00:02:41 +0200

    affichage html

--- script/main.js
+++ script/main.js
@@ -1,61 +1,134 @@
+const clickButton = document.getElementById("demarrer");
 const inputLettre = document.getElementById('lettre');
+const lblLettre =  document.getElementById('lblLettre');
+const zoneDeJeu = document.getElementById('zoneJeu');
+const Message = document.getElementById('Message');
+const imagePendu = document.getElementById('imagePendu');
+const buttonQuitter = document.getElementById('quitter');
+const zoneFin = document.getElementById('zoneFin');
+const afficheLettreUtilisee = document.getElementById('lettresUtilisees');
+const motTrouve = document.getElementById('motTrouve');
+
 const alphabet = 'abcdefghijklmnopqrstuvwxyz';
-let lettre =""
+let lettre ="";
 let erreurs_commises = 0;
-let erreurs_autorisees = 7;
+let erreurs_autorisees = 6;
+let lettresUtilisees = [];
+let mot_a_trouver = "";
+let mot_trouve = "";
+let nbCoup = 0;
+let listeResultats = [];
+
+let divErreur = document.createElement('div');
+divErreur.innerHTML = 'on attend une lettre';
 
 
-function demanderLettre(){
-    lettre = " ";
-    while(!alphabet.includes(lettre) || lettre.length>1){
-        lettre = prompt('choisissez une lettre');
-    }
-    return lettre
-}
-
-function lancerjeu(){
+function init(){
     // je défini mon mot à trouver de manière aléatoire dans la liste wordsArray
     let alea = Math.floor(Math.random() * wordsArray.length);
-    let mot_a_trouver = wordsArray[alea];
-    let longueur = mot_a_trouver.length;
+    mot_a_trouver = wordsArray[alea];
+    mot_trouve = "";
+    mot_trouve = '-'.repeat(mot_a_trouver.length);
+    inputLettre.innerHTML = "";
+    lettresUtilisees = [];
+    erreurs_commises = 0;
+    imagePendu.src = `/images/pendu${7-erreurs_commises}.png`;
+    Message.innerHTML = "";
+    motTrouve.innerHTML = mot_trouve;
+    afficheLettreUtilisee.innerHTML = "";
+}
+
+function finJeu(resultat){
+    zoneDeJeu.classList.add('hidden');
+    clickButton.classList.remove('hidden');
+    buttonQuitter.classList.remove('hidden');
+    if(resultat){
+        Message.innerHTML = `Vous avez gagné en ${nbCoup} coups`;
+        listeResultats.push(nbCoup);
+    }else{
+        Message.innerHTML = `Vous avez perdu en ${nbCoup} coups`;
+    }
+}
+function lancerjeu(){
     // je créé le mot trouvé par l'utilisateur :
-    let mot_trouve = '-'.repeat(longueur);
+    
     console.log('mot à trouver', mot_a_trouver);
-    // lettre = prompt('choisissez une lettre');
-    
-    lettre = demanderLettre();
-    
-    
-    while(mot_a_trouver != mot_trouve && erreurs_commises < erreurs_autorisees){
+
+    // Boucle du jeu
+    if(verifierLettre(lettre)){
+        nouveauMot = "";
         if(mot_a_trouver.includes(lettre)){
-            let nouveauMotTrouve =""
             for(let i = 0 ; i < mot_a_trouver.length ; i++){
+                
                 if(mot_a_trouver[i] === lettre){
-                    nouveauMotTrouve +=lettre;
+                    nouveauMot += lettre;
                 }else{
-                    if(mot_trouve[i] === '-'){
-                        nouveauMotTrouve += '-';
-                    }else{
-                        
-                        nouveauMotTrouve += mot_trouve[i];
-                    }
+                    
+                    nouveauMot += mot_trouve[i];
                 }
             }
-            mot_trouve = nouveauMotTrouve;
-            
-            
-            console.log(mot_trouve);
-            
-        } else {
+            mot_trouve = nouveauMot;
+            motTrouve.innerHTML = mot_trouve;
+            inputLettre.value = "";
+        }else{
             erreurs_commises++;
-            
+            imagePendu.src = `/images/pendu${7-erreurs_commises}.png`;
+            inputLettre.value = "";
         }
-        lettre = demanderLettre();
-        console.log('erreurs', erreurs_commises);
-        
+        nbCoup++;
     }
-console.log('sortie');
+
+    // Condition victoire
+    if(mot_a_trouver === mot_trouve){
+        finJeu(true);
+    // Condition défaite
+    }else if(erreurs_commises>erreurs_autorisees){
+        finJeu(false);
+    }
 
 }
 
- lancerjeu();
+
+
+function verifierLettre(lettre){
+    Message.innerHTML = "";
+    if(lettresUtilisees.includes(lettre)){
+        Message.innerHTML = 'vous avez déjà utilisé cette lettre';
+        inputLettre.value ="";
+        return false;
+    }else if(!alphabet.includes(lettre)){
+        Message.innerHTML = 'veuillez saisir une lettre';
+        inputLettre.value = "";
+        return false;
+    }else {
+        lettresUtilisees.push(lettre);
+        afficheLettreUtilisee.innerHTML = `Voici les lettres déjà utilisées : ${lettresUtilisees}`;
+        return true;
+    }
+}
+
+const onClickDemarrer = clickButton.addEventListener('click', ()=>{
+    clickButton.classList.add('hidden');
+    buttonQuitter.classList.add('hidden');
+    zoneDeJeu.classList.remove('hidden');
+    init();
+})
+
+const onChangeInput = inputLettre.addEventListener('input' , ()=>{
+    lettre = inputLettre.value;
+    lancerjeu();
+})
+
+const clickQuitter = buttonQuitter.addEventListener('click' , ()=>{
+    buttonQuitter.classList.add('hidden');
+    clickButton.classList.add('hidden');
+    zoneFin.classList.remove('hidden');
+    afficheLettreUtilisee.innerHTML = "";
+    let somme= 0;
+    Message.innerHTML = "";
+    for(i=0 ; i<listeResultats.length ; i++){
+        
+        somme += listeResultats[i];
+    }
+    zoneFin.innerHTML =`Vous avez eu une moyenne de  ${Math.floor(somme/(listeResultats.length))}`;
+})
